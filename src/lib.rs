bf9ba529adc98035288231dc3dd4afd5123ea236
Luis Gerhorst <luis.gerhorst@fau.de>
2019-09-17 20:40:22 +0200

    Update linux-kernel-module-rust lib

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,6 @@ impl FileSystem for BFS {
     // https://github.com/gereeter/llvm-safe/blob/9710754e107db61965f257ed9d6c55e311dd32aa/examples/kaleidoscope_lib/trans.rs
     // uses a separate ConstCStr.
     const NAME: &'static CStr = unsafe { &*("bfs-rust\x00" as *const str as *const CStr) };
-
-    // TODO: Which flags are the default?
     const FLAGS: FileSystemFlags = FileSystemFlags::FS_REQUIRES_DEV;
 }
 
@@ -41,12 +39,32 @@ impl linux_kernel_module::KernelModule for BFSModule {
     }
 }
 
-// impl Drop for BFSModule {
-//     fn drop(&mut self) {
-//         println!("My message is {}", self.message);
-//         println!("Goodbye kernel module!");
-//     }
+// extern "C" fn mount_callback<BFS>(
+//     fs_type: *mut bindings::file_system_type,
+//     flags: c_types::c_int,
+//     dev_name: *const c_types::c_char,
+//     data: *mut c_types::c_void,
+// ) -> *mut bindings::dentry {
+//     unsafe { bindings::mount_bdev(fs_type, flags, dev_name, Some(fill_super_callback::<BFS>)) }
 // }
+
+// extern "C" fn fill_super_callback<BFS>(
+//     sb: *mut bindings::super_block,
+//     data: *mut c_types::c_void,
+//     silent: c_types::c_int,
+// ) -> c_types::c_int {
+//     // T::fill_super(...)
+//     // This should actually create an object that gets dropped by
+//     // file_system_registration::kill_sb. You can point to it with
+//     // sb->s_fs_info.
+//     unimplemented!();
+// }
+
+impl Drop for BFSModule {
+    fn drop(&mut self) {
+        println!("Goodbye kernel module!");
+    }
+}
 
 linux_kernel_module::kernel_module!(
     BFSModule,
