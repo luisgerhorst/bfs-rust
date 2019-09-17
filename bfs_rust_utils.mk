1d427c2448e9441619f4b77cb718ee0249446c61
Luis Gerhorst <luis.gerhorst@fau.de>
2019-09-17 15:13:33 +0200

    bfs_rust_utils.mk

---
+++ bfs_rust_utils.mk
@@ -0,0 +1,47 @@
+LOOP_IMAGE = loop0-img
+LOOP_DEV = /dev/loop0
+LOOP_MOUNTPOINT = loop0-mp
+
+# Module
+
+insmod:
+	insmod /lib/modules/5.0.19-yocto-standard/extra/bfsrust.ko
+
+rmmod:
+	rmmod --syslog bfsrust # --syslog quiets the "could not open builtin file" error
+
+# Image
+
+$(LOOP_IMAGE):
+	touch $(LOOP_IMAGE) \
+	&& dd bs=4096 count=2000 if=/dev/zero of="$(LOOP_IMAGE)" \
+	&& losetup $(LOOP_DEV) $(LOOP_IMAGE)
+
+$(LOOP_MOUNTPOINT):
+	mkdir $(LOOP_MOUNTPOINT)
+
+erase:
+	losetup -d $(LOOP_DEV)
+	rm $(LOOP_IMAGE) \
+	; rm -rfd $(LOOP_MOUNTPOINT)
+
+hexdump:
+	hexdump $(LOOP_DEV) -C
+
+# Mounting
+
+mount: $(LOOP_IMAGE) $(LOOP_MOUNTPOINT)
+	mount -o loop -t bfs-rust $(LOOP_DEV) $(LOOP_MOUNTPOINT)
+
+umount:
+	umount $(LOOP_MOUNTPOINT)
+
+remount: | umount mount
+
+# Load/Unload/Reload
+
+load: | insmod mount
+unload: | umount rmmod
+reload: | unload load
+
+erase-reload: | umount erase rmmod load
