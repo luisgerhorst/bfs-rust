54728a62270ff44e56b517dd8e24a15573b86296
Luis Gerhorst <privat@luisgerhorst.de>
2019-09-22 23:11:02 +0200

    const cstr!(), Sync filesystem::Registration<T>

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,6 @@
 
 extern crate alloc;
 
-use alloc::borrow::ToOwned;
-use alloc::string::String;
-
 use linux_kernel_module::{self, println, cstr, CStr};
 use linux_kernel_module::filesystem::{self, FileSystem, FileSystemFlags};
 
@@ -14,16 +11,10 @@ struct BFSModule {
     _fs_registration: filesystem::Registration<BFS>
 }
 
-// This is safe because Registration doesn't actually expose any methods. TODO: Sure?
-unsafe impl Sync for BFSModule {}
-
 struct BFS {}
 
 impl FileSystem for BFS {
-    // TODO: Is there a better way to do this?
-    // https://github.com/gereeter/llvm-safe/blob/9710754e107db61965f257ed9d6c55e311dd32aa/examples/kaleidoscope_lib/trans.rs
-    // uses a separate ConstCStr.
-    const NAME: &'static CStr = unsafe { &*("bfs-rust\x00" as *const str as *const CStr) };
+    const NAME: &'static CStr = cstr!("bfs-rust");
     const FLAGS: FileSystemFlags = FileSystemFlags::FS_REQUIRES_DEV;
 }
 
