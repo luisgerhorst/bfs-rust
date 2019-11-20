05dd7a0b0f4a46c725eb844b43efff0f080b9b0a
Luis Gerhorst <privat@luisgerhorst.de>
2019-11-21 00:15:31 +0100

    Use safe fill_super

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,16 @@
 
 extern crate alloc;
 
-use linux_kernel_module::{self, println, cstr, CStr};
+use alloc::boxed::Box;
+use linux_kernel_module::{self, println, cstr, CStr, bindings,
+                          c_types, KernelModule, KernelResult};
 use linux_kernel_module::filesystem::{self, FileSystem, FileSystemFlags};
-use linux_kernel_module::{bindings, c_types};
 
 struct BFSModule {
     _fs_registration: filesystem::Registration<BFS>
 }
 
 struct BFS {}
-
 
 // Start of code derived from bfs.h
 
@@ -24,33 +24,42 @@ struct BFS {}
 // mkfs.bfs(8) should really limit its -N option to 511 and not 512. For now,
 // we just print a warning if a filesystem is mounted with such "impossible
 // to fill up" number of inodes
-const BFS_MAX_LASTI = 513;
+const BFS_MAX_LASTI: u64 = 513; // TODO: u64?
 
 // Assertion: unsigned long == u64.
-struct BFS_SB_INFO {
-    u64 si_blocks;
-    u64 si_freeb;
-    u64 si_freei;
-    u64 si_lf_eblk;
-    u64 si_lasti;
+struct Inner {
+    blocks: u64,
+    freeb: u64,
+    freei: u64,
+    lf_eblk: u64,
+    lasti: u64,
     // TODO:
-    //
     // DECLARE_BITMAP(si_imap, BFS_MAX_LASTI+1);
-    // struct mutex bfs_lock;
-    //
-    // See https://github.com/torvalds/linux/blob/master/fs/bfs/bfs.h#L27
+}
+
+struct BFSSuperBlockInfo {
+    // inner: Mutex<Inner>,
 }
 
 // End of code derived from bfs.h
 
-
 impl FileSystem for BFS {
     const NAME: &'static CStr = cstr!("bfs-rust");
     const FLAGS: FileSystemFlags = FileSystemFlags::FS_REQUIRES_DEV;
+
+    type SuperBlockInfo = BFSSuperBlockInfo;
+
+    fn fill_super(fs_info: &mut Option<Box<Self::SuperBlockInfo>>) -> KernelResult<()> {
+        *fs_info = Some(Box::new(BFSSuperBlockInfo {
+
+        }));
+
+        Ok(())
+    }
 }
 
-impl linux_kernel_module::KernelModule for BFSModule {
-    fn init() -> linux_kernel_module::KernelResult<Self> {
+impl KernelModule for BFSModule {
+    fn init() -> KernelResult<Self> {
         println!("Hello kernel module!");
 
         let fs_registration = filesystem::register::<BFS>()?;
@@ -59,43 +68,6 @@ impl linux_kernel_module::KernelModule for BFSModule {
             _fs_registration: fs_registration,
         })
     }
-}
-
-// TODO: Make this a method of FileSystem with a default implementation?
-extern "C" fn mount_callback<BFS>(
-    fs_type: *mut bindings::file_system_type,
-    flags: c_types::c_int,
-    dev_name: *const c_types::c_char,
-    data: *mut c_types::c_void,
-) -> *mut bindings::dentry {
-    unsafe {
-        bindings::mount_bdev(fs_type, flags, dev_name, data,
-                             Some(fill_super_callback::<BFS>))
-    }
-}
-
-extern "C" fn fill_super_callback<BFS>(
-    _sb: *mut bindings::super_block,
-    _data: *mut c_types::c_void,
-    _silent: c_types::c_int,
-) -> c_types::c_int {
-    // T::fill_super(...)
-    // This should actually create an object that gets dropped by
-    // file_system_registration::kill_sb. You can point to it with
-    // sb->s_fs_info.
-    //
-    // TODO: Is this really true? Isn't a fs_info destroyed when the filesystem
-    // is unmounted?
-
-    // TODO:
-    //
-    // 1. Set sb->s_fs_info to an in-memory structure representing the
-    // superblock.
-    // 2. s_time_min = 0, s_time_max = U32_MAX.
-    // 3. ..., see https://github.com/torvalds/linux/blob/master/fs/bfs/inode.c#L330
-
-    println!("Hello from fill_super_callback");
-    unimplemented!();
 }
 
 impl Drop for BFSModule {
